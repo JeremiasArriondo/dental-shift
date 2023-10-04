c837b37bbe1c14693f35c6c3aa03db3863eeddbd
Jeremias Arriondo <jeremiasarriondo98@gmail.com>
2023-10-04 02:15:52 -0300

    feat: updated main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+import { Contact } from "@/components/contact";
+import { Hero } from "@/components/hero";
+
 export const metadata = {
   title: "Inicio",
   description: "Conoce más sobre mis servicios odontológicos y de salud dental",
@@ -5,10 +8,9 @@ export const metadata = {
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between ">
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        Consultorio dental
-      </div>
+    <main className="flex min-h-screen flex-col items-center">
+      <Hero />
+      <Contact />
     </main>
   );
 }
---
+++ components/hero.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import { Button } from "./ui/button";
+
+export const Hero = () => {
+  return (
+    <section className="min-h-[calc(100vh-72px)] mx-8 py-8 flex items-center gap-8 flex-col md:flex-row">
+      <div>
+        <h1 className="text-5xl font-semibold">
+          Odontología de Calidad: <br />
+          Tu Sonrisa en Buenas Manos
+        </h1>
+        <p className="md:w-[75%] mt-3 text-gray-500">
+          Descubre la odontología de calidad de la mano de una profesional
+          apasionada por tu sonrisa. En mi consulta, tu bienestar bucal es mi
+          prioridad. Desde limpiezas regulares hasta tratamientos avanzados, te
+          ofrezco una atención personalizada y de calidad. Tu sonrisa está en
+          buenas manos.
+        </p>
+        <div className="flex gap-4 my-4">
+          <Button>Conoce más</Button>
+          <Button variant="secondary">Agenda un turno</Button>
+        </div>
+      </div>
+      <Image
+        src="/images/sonrisa-hero.avif"
+        height={500}
+        width={600}
+        className="rounded-3xl object-cover"
+        alt="Sonrisa de mujer"
+        priority
+        style={{
+          width: "100%",
+          height: "auto",
+        }}
+      />
+    </section>
+  );
+};
