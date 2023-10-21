6d4fabaa5f4357bd1c758256bd016ca937f9ccfa
Jeremias Arriondo <jeremiasarriondo98@gmail.com>
2023-10-21 19:12:42 -0300

    feat: align hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -3,36 +3,40 @@ import { Button } from './ui/button'
 
 export const Hero = () => {
   return (
-    <section className="min-h-[calc(100vh-72px)] mx-4 md:mx-8 py-8 flex items-center gap-8 flex-col md:flex-row">
-      <div>
-        <h1 className="text-5xl font-semibold">
-          Odontología de Calidad: <br />
-          Tu Sonrisa en Buenas Manos
-        </h1>
-        <p className="md:w-[75%] mt-3 text-gray-500">
-          Descubre la odontología de calidad de la mano de una profesional
-          apasionada por tu sonrisa. En mi consulta, tu bienestar bucal es mi
-          prioridad. Desde limpiezas regulares hasta tratamientos avanzados, te
-          ofrezco una atención personalizada y de calidad. Tu sonrisa está en
-          buenas manos.
-        </p>
-        <div className="flex gap-4 my-4">
-          <Button>Conoce más</Button>
-          <Button variant="secondary">Agenda un turno</Button>
+    <section className="min-h-[calc(100vh-72px)] max-w-screen-xl flex items-center xl:mx-auto p-8">
+      <div className="grid md:grid-cols-2 items-center gap-8">
+        <div>
+          <h1 className="text-5xl font-semibold">
+            Odontología de Calidad: <br />
+            Tu Sonrisa en Buenas Manos
+          </h1>
+          <p className="md:w-[75%] mt-3 text-gray-500">
+            Descubre la odontología de calidad de la mano de una profesional
+            apasionada por tu sonrisa. En mi consulta, tu bienestar bucal es mi
+            prioridad. Desde limpiezas regulares hasta tratamientos avanzados,
+            te ofrezco una atención personalizada y de calidad. Tu sonrisa está
+            en buenas manos.
+          </p>
+          <div className="flex gap-4 my-4">
+            <Button>Conoce más</Button>
+            <Button variant="secondary">Agenda un turno</Button>
+          </div>
+        </div>
+        <div className="w-full">
+          <Image
+            src="/images/sonrisa-hero.avif"
+            height={300}
+            width={600}
+            className="rounded-3xl object-cover"
+            alt="Sonrisa de mujer"
+            priority
+            style={{
+              width: '100%'
+              // height: 'auto'
+            }}
+          />
         </div>
       </div>
-      <Image
-        src="/images/sonrisa-hero.avif"
-        height={500}
-        width={600}
-        className="rounded-3xl object-cover"
-        alt="Sonrisa de mujer"
-        priority
-        style={{
-          width: '100%',
-          height: 'auto'
-        }}
-      />
     </section>
   )
 }
