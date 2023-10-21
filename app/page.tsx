c73e0f68f2f3def8d6fd9ed7d5e17fb8e7c1756c
Jeremias Arriondo <jeremiasarriondo98@gmail.com>
2023-10-21 19:12:53 -0300

    feat: hero min-h

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export const metadata = {
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center">
+    <main className="min-h-screen">
       <Hero />
       <Services />
       <Contact />
