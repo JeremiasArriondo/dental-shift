3fcb4ed4d0397d88d27a5ca181f689bdf8a056e5
Jeremias Arriondo <jeremiasarriondo98@gmail.com>
2023-10-29 15:13:05 -0300

    fix: suppress warning extensions

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body className={inter.className} suppressHydrationWarning>
         <div className="flex relative min-h-screen flex-col justify-between">
           {children}
         </div>
