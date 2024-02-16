c25ea66032ef615932735b734841dac37320180b
Jeremias Arriondo <jeremiasarriondo98@gmail.com>
2024-02-16 01:38:01 -0300

    fix: order layout admin

---
+++ app/dashboard/(clients)/layout.tsx
@@ -0,0 +1,51 @@
+import { redirect } from 'next/navigation'
+import { MainNav } from '@/components/main-nav'
+import { DashboardNav } from '@/components/nav'
+import { UserAccountNav } from '@/components/user-account-nav'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { Database } from '@/types/database'
+import { cookies } from 'next/headers'
+import { dashboardConfig } from '@/config/dashboard'
+
+interface DashboardLayoutProps {
+  children?: React.ReactNode
+}
+
+export default async function DashboardLayout({
+  children
+}: DashboardLayoutProps) {
+  const supabase = createServerComponentClient<Database>({ cookies })
+  const {
+    data: { session }
+  } = await supabase.auth.getSession()
+
+  if (session === null) {
+    redirect('/login')
+  }
+
+  return (
+    <div className="flex min-h-screen flex-col space-y-6">
+      <header className="sticky top-0 z-40 border-b bg-bgColor/60 backdrop-filter backdrop-blur-3xl backdrop-saturate-200">
+        <div className="flex h-16 items-center justify-between py-4 px-8">
+          <MainNav items={dashboardConfig.mainNav} />
+          <UserAccountNav
+            user={{
+              name: session.user.user_metadata.name,
+              image: session.user.user_metadata.avatar_url,
+              email: session.user.email!
+            }}
+          />
+        </div>
+      </header>
+
+      <div className="grid mx-8 gap-12 md:grid-cols-[200px_1fr]">
+        <aside className="hidden w-[200px] flex-col md:flex">
+          <DashboardNav items={dashboardConfig.sidebarNav} />
+        </aside>
+        <main className="flex w-full flex-1 flex-col overflow-hidden">
+          {children}
+        </main>
+      </div>
+    </div>
+  )
+}
