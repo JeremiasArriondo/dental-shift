1f1f553a07fc112cafb6d8592add6e3c9d0c4f17
Jeremias Arriondo <jeremiasarriondo98@gmail.com>
2025-02-20 11:41:06 -0300

    styles: gradient add

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -8,7 +8,7 @@ export default async function MarketingLayout({
   children
 }: MarketingLayoutProps) {
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="flex min-h-screen flex-col bg-gradient-to-l from-white via-green-50 to-white">
       <Navbar />
       <main className="flex-1">{children}</main>
     </div>
