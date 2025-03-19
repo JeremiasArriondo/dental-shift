15a52b86dafdc98d0be1a24a2cbecef0e08037c5
Jeremias Arriondo <jeremiasarriondo98@gmail.com>
2025-03-19 14:11:53 -0300

    feat: logic for sent email each day

--- app/api/email-daily/route.ts
+++ app/api/email-daily/route.ts
@@ -1,8 +1,21 @@
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 import nodemailer from 'nodemailer'
 
-export async function POST(req: Request) {
-  // const { firstName, date } = await req.json()
+export async function POST() {
+  const supabase = createRouteHandlerClient({ cookies })
+  const today = new Date().toISOString().split('T')[0]
+
+  const { data: recipients, error } = await supabase
+    .from('turnos')
+    .select('*, users(name, email)')
+    .gte('appointment_date', `${today}T00:00:00.000Z`)
+    .lt('appointment_date', `${today}T23:59:59.999Z`)
+
+  if (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 })
+  }
 
   try {
     let transporter = nodemailer.createTransport({
@@ -10,26 +23,53 @@ export async function POST(req: Request) {
       port: 587,
       pool: true,
       auth: {
-        user: process.env.EMAIL_USER, // Tu correo
-        pass: process.env.EMAIL_PASS // Tu contraseña de aplicación
-      }
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+      },
+      maxMessages: Infinity,
+      maxConnections: 5
     })
 
-    const mailOptions = {
-      from: process.env.EMAIL_USER, // Sender address from the .env file
-      to: 'jeremiascabj@gmail.com',
-      subject: 'Sending Email using Node.js',
-      text: 'That was easy!'
-    }
+    const emailPromises = recipients.map((recipient) =>
+      transporter.sendMail({
+        from: `"Eliana Ginocchio" <${process.env.EMAIL_USER}>`,
+        to: `${recipient.users.name} <${recipient.users.email}>`,
+        subject: '📅 Recordatorio de Turno',
+        text: `Hola ${recipient.users.name}, tienes un turno programado para hoy a las ${recipient.hour}hs.`,
+        html: `
+            <!DOCTYPE html>
+            <html>
+            <head>
+              <meta charset="UTF-8">
+              <title>📅 Recordatorio de Turno</title>
+            </head>
+            <body>
+              <div style="font-family: Arial, sans-serif; padding: 20px;">
+                <h2 style="color: #4F7268;">📅 Recordatorio de Turno</h2>
+                <p>Hola <strong>${recipient.users.name}</strong>,</p>
+                <p>Este es un recordatorio de tu turno programado.</p>
+                <div style="background: #f0f0f0; padding: 10px; border-radius: 5px;">
+                  📍 <strong>Fecha:</strong> ${recipient.date} <br>
+                  ⏰ <strong>Hora:</strong> ${recipient.hour}hs <br>
+                </div>
+              </div>
+            </body>
+            </html>`
+      })
+    )
 
-    // const info = await transporter.sendEmail(mailOptions)
-    transporter.sendMail(mailOptions, function (error, info) {
-      if (error) {
-        console.log('Error:', error)
-      } else {
-        console.log('Email sent: ', info.response)
-      }
-    })
+    Promise.all(emailPromises)
+      .then((results) => {
+        console.log('All emails sent successfully')
+        results.forEach((result) => {
+          console.log(
+            `Message to ${result.envelope.to} sent: ${result.messageId}`
+          )
+        })
+      })
+      .catch((errors) => {
+        console.error('Failed to send one or more emails:', errors)
+      })
 
     return NextResponse.json(
       { message: 'Email sent successfully' },
